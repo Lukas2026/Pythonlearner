2138307f29196a06e54a540de4f8e574dc69e9d8
Lukas2026 <62200410+Lukas2026@users.noreply.github.com>
2020-03-21 10:19:13 +0100

    Add files via upload

---
+++ dungeon003.py
@@ -0,0 +1,133 @@
+import random
+class Game:
+	zoo = {}
+	
+class Monster:
+	number = 0
+
+	def __init_(self):
+		print('A new monster has been created')
+		self.number = Monster.number
+		Monster.number += 1
+		Game.zoo[self.number] = self # put monster in zoo
+		self.hp = random.randint(50, 80)
+		self.attack  = random.randint(1, 6)
+		self.defense = random.randint(1, 6)
+		self.damage = random.randint(1, 6)
+		
+	def __str__(self):
+		return "I am Monster Number #{} my stats are: {} hp, {} att, {} def, {} dam".format(self.number, self.hp, self.attack, self.defense, self.damage)
+#build 10 monsters
+for _ in range(10):
+	Monster()
+	
+#for m in Game.zoo.values():
+	#print(m)
+
+player = '@'
+hp = 20
+gold = 0
+food = 0
+px,py,pz = 3,3,0
+level1 = '''
+#############################################################
+#...........................................................#
+#.....................................$.....................#
+#...................f...............................$.......#
+#...........................................................#
+#.....$...........................f.........................#
+#...........................................................#
+#............f.......................................f......#
+#...........................f.........$.....................#
+#...........................................................#
+#############################################################'''
+level2 = '''
+#############################################################
+#...........................................................#
+#........................................f.............$....#
+#.......$.......f..............$............................#
+#...........................................................#
+#........................................$.......f..........#
+#...........................................................#
+#..........$......f.........f.......$..............f........#
+#...........................................................#
+#...........................................................#
+############################.################################
+#####################.................#######################
+#####################.................#######################
+#############################################################'''
+
+#create dungeon
+dungeon = []
+for raw in (level1,level2):
+    level = []
+    for line in raw.splitlines():
+        level.append(list(line))
+    dungeon.append(level)
+    
+print(dungeon)
+
+print(dungeon[0][2])
+    
+# # ..... rock
+# f ...... food
+# $ ....... gold
+message = ''
+while hp > 0:
+    for y,line in enumerate(dungeon[pz]):
+        for x,char in enumerate(line):
+            if x == px and y == py:
+                print(player, end = '')
+            else:
+                print(char,end = '')
+        print()
+    print(message)
+    command = input('{} hp, {} $, {} f >>>'.format(hp,gold,food))
+    message = ''
+    deltax = 0 #wo der spieler gehen will
+    deltay = 0
+    if command == 'a':
+        deltax = -1
+    if command == 'd':
+        deltax = 1
+    if command == 'w':
+        deltay = -1 #y achse lauft nach unten
+    if command == 's':
+        deltay = 1
+    #change level
+    if command == 'climb up':
+        pz -= 1
+        if pz < 0:
+            pz = 0
+    if command == 'climb down':
+        pz += 1
+        if pz > len(dungeon) -1:
+            pz = len(dungeon) -1
+    #check ob in felsen
+    target = dungeon[pz][py + deltay][px + deltax]
+    if target == '#':
+        message += 'ouch'
+        hp -= 1
+        deltax = 0
+        deltay = 0
+       
+    #movement
+    px += deltax
+    py += deltay
+    #picking up itmes
+    target = dungeon[pz][py][px]
+    if target == '$':
+        message += 'You found Gold!'
+        gold += 1
+        dungeon[pz][py][px] = '.'
+    if target == 'f':
+        message += 'You Found Food'
+        food += 1
+        dungeon[pz][py][px] = '.'
+        
+        
+        
+
+
+print('Game over')
+    
